41f644bb97d255d7cc7931e5cd2166826fb88c1f
Samantha Fisher <samantha.fisher@edalex.com>
2020-08-26 12:40:44 +1000

    Apply whitespace changes

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -2,7 +2,7 @@
 namespace block_equella_tasks\privacy;
 
 class provider implements \core_privacy\local\metadata\null_provider {
-    //this plugin does not store any personal user data.
+    // this plugin does not store any personal user data.
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
